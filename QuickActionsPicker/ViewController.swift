be3eab80e03b644496e82adcb7f6d1a83c1f599a
Yuri Karabatov <yuri@yurikarabatov.com>
2018-11-23 18:09:42 +0300

    Add a quick actions panel above the action sheet

--- QuickActionsPicker/ViewController.swift
+++ QuickActionsPicker/ViewController.swift
@@ -19,18 +19,37 @@ final class ViewController: UIViewController {
     @IBAction func buttonTapped() {
         let rows = ["10 g", "100 g"]
 
-        ActionSheetStringPicker.show(
-            withTitle: "Tomato",
+        let asPicker = ActionSheetStringPicker.init(
+            title: "Tomato",
             rows: rows,
             initialSelection: 1,
             doneBlock: { [weak self] picker, index, value in
                 self?.button.setTitle(rows[index], for: UIControl.State.normal)
             },
-            cancel: { picker in
-                // Do nothing
-            },
+            cancel: nil,
             origin: button
         )
+
+        guard let picker = asPicker else { return }
+
+        let quickPanel = UIView()
+        quickPanel.backgroundColor = UIColor.red
+        quickPanel.translatesAutoresizingMaskIntoConstraints = false
+
+        // Picker subviews are created only after we try to show it.
+        picker.show()
+
+        // We will add our panel to the toolbar's superview and constrain it
+        // to the top of the toolbar.
+        guard let canvas = picker.toolbar.superview else { return }
+
+        canvas.addSubview(quickPanel)
+        canvas.addConstraints([
+            quickPanel.heightAnchor.constraint(equalToConstant: 72.0),
+            quickPanel.bottomAnchor.constraint(equalTo: picker.toolbar.topAnchor),
+            quickPanel.leftAnchor.constraint(equalTo: picker.toolbar.leftAnchor),
+            quickPanel.rightAnchor.constraint(equalTo: picker.toolbar.rightAnchor)
+        ])
     }
 }
 
