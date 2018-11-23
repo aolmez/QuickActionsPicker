1292e095477930def334d4f66284b1ea54142b65
Yuri Karabatov <yuri@yurikarabatov.com>
2018-11-23 19:27:00 +0300

    Add a stack view with quick actions to the panel

--- QuickActionsPicker/ViewController.swift
+++ QuickActionsPicker/ViewController.swift
@@ -36,6 +36,40 @@ final class ViewController: UIViewController {
         quickPanel.backgroundColor = UIColor.red
         quickPanel.translatesAutoresizingMaskIntoConstraints = false
 
+        let actionStack = UIStackView()
+        actionStack.axis = .horizontal
+        actionStack.distribution = .fill
+        actionStack.translatesAutoresizingMaskIntoConstraints = false
+
+        let quickActions = ["10 g", "20 g", "50 g", "200 g"]
+        let separator = "|"
+
+        // Only take the first four items.
+        for action in quickActions.prefix(through: 3) {
+            if !actionStack.arrangedSubviews.isEmpty {
+                let sepLabel = UILabel()
+                sepLabel.text = separator
+                sepLabel.setContentHuggingPriority(UILayoutPriority.defaultHigh, for: NSLayoutConstraint.Axis.horizontal)
+                actionStack.addArrangedSubview(sepLabel)
+            }
+
+            let button = UIButton(type: UIButton.ButtonType.custom)
+            button.setTitle(action, for: UIControl.State.normal)
+            actionStack.addArrangedSubview(button)
+
+            if let firstButton = actionStack.arrangedSubviews.first as? UIButton {
+                button.widthAnchor.constraint(equalTo: firstButton.widthAnchor).isActive = true
+            }
+        }
+
+        quickPanel.addSubview(actionStack)
+        quickPanel.addConstraints([
+            actionStack.topAnchor.constraint(equalTo: quickPanel.topAnchor),
+            actionStack.leftAnchor.constraint(equalTo: quickPanel.leftAnchor),
+            actionStack.bottomAnchor.constraint(equalTo: quickPanel.bottomAnchor),
+            actionStack.rightAnchor.constraint(equalTo: quickPanel.rightAnchor),
+        ])
+
         // Picker subviews are created only after we try to show it.
         picker.show()
 
@@ -45,7 +79,7 @@ final class ViewController: UIViewController {
 
         canvas.addSubview(quickPanel)
         canvas.addConstraints([
-            quickPanel.heightAnchor.constraint(equalToConstant: 72.0),
+            // quickPanel.heightAnchor.constraint(equalToConstant: 72.0),
             quickPanel.bottomAnchor.constraint(equalTo: picker.toolbar.topAnchor),
             quickPanel.leftAnchor.constraint(equalTo: picker.toolbar.leftAnchor),
             quickPanel.rightAnchor.constraint(equalTo: picker.toolbar.rightAnchor)
